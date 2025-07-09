a85f1d1511b127a45715b4edb1f54e4424d9d7c0
Valvixx <valvixx04@gmail.com>
2025-07-10 01:21:29 +0300

    Adding Traits and Spell Features(Bestows a bonus)

--- src/components/InputFields.vue
+++ src/components/InputFields.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch, defineProps, defineEmits } from 'vue'
+import {defineEmits, defineProps, ref, watch} from 'vue'
 
 const props = defineProps({
   modelValue: String
@@ -13,7 +13,7 @@ const spellEffects = ref([
 ])
 
 const spellDamage = ref([
-  {intValue: '0', addValue: '0', type: '', spellModifiers: [] },
+  {intValue: '', addValue: '', type: '', spellModifiers: [] },
 ])
 
 
@@ -45,8 +45,8 @@ function deleteSpellEffect() {
 
 function addDamage() {
   spellDamage.value.push({
-    intValue: '0',
-    addValue: '0',
+    intValue: '',
+    addValue: '',
     type: '',
     spellModifiers: []
   })
@@ -63,12 +63,18 @@ function deleteModifier(index) {
   spellDamage.value[index].spellModifiers.pop()
 }
 
-function onWheel(event, obj, key, step = 1) {
-  event.preventDefault() // предотвращает прокрутку страницы
+function onWheel(event, obj, key, step = 1, min = -9999, max = 9999) {
+  event.preventDefault()
 
   const delta = Math.sign(event.deltaY) * step
   const current = parseInt(obj[key]) || 0
-  obj[key] = (current - delta).toString()
+  let next = current - delta
+
+  // Ограничение значений в заданном диапазоне
+  next = Math.min(Math.max(next, min), max)
+
+  obj[key] = next.toString()
+
 }
 
 </script>
@@ -125,9 +131,9 @@ function onWheel(event, obj, key, step = 1) {
 
     <div v-for="(Damage, index) in spellDamage" :key="index" class="damage-container">
       <div class="damage-subcontainer">
-        <input @wheel="onWheel($event, Damage, 'intValue')" v-model="Damage.intValue" type="number" class="damage">
+        <input @wheel="onWheel($event, Damage, 'intValue', 1, 0)" v-model="Damage.intValue" type="number" placeholder="Dice dmg" class="damage" min="0">
         <!--      <h3 class="letter">d+</h3>-->
-        <input @wheel="onWheel($event, Damage, 'addValue')" v-model="Damage.addValue" type="number" class="damage">
+        <input @wheel="onWheel($event, Damage, 'addValue')" v-model="Damage.addValue" type="number" placeholder="Flat dmg" class="damage">
         <select v-model="Damage.type" class="input effect">
           <option disabled value="">-- Select a type of damage --</option>
           <option v-for="type in damageTypes" :key="type">{{type}}</option>
@@ -137,9 +143,41 @@ function onWheel(event, obj, key, step = 1) {
       </div>
 
       <div v-for="(modifier, index2) in Damage.spellModifiers" :key = "index2" class="modifierWrapper">
+        <input @wheel="onWheel($event, modifier, 'value', 5)" v-model="modifier.value" type="number" class="input"  id="modifier-value" placeholder="Value (%)" step="5" >
         <input v-model="modifier.name" class="input"  id="modifier-name" placeholder="Modifier Name">
-        <input @wheel="onWheel($event, modifier, 'value', 5)" v-model="modifier.value" type="number" class="input"  id="modifier-value" placeholder="Modifier Value (Percents)" step="5" >
       </div>
+    </div>
+
+    <div class="head-3">
+      <h3>Traits</h3>
+    </div>
+
+    <div class="buttonWrapper">
+      <button class="add-effect-btn" @click="deleteDamage">Delete Trait</button>
+      <button class="add-effect-btn" @click="addDamage">Add Trait</button>
+    </div>
+
+    <div class="traits-container">
+      <input class="input" id="trait-value" type="number" placeholder="Value (pts)">
+      <input class="input" id="trait-name" placeholder="Trait Name">
+      <button @click="deleteModifier(index)" class="modifier-btn"> - Modifier</button>
+      <button @click="addModifier(index)" class="modifier-btn"> + Modifier</button>
+    </div>
+
+    <div class="head-3">
+      <h3>Spell Features</h3>
+    </div>
+
+    <div class="head-4">
+      <h4>Bestows a Bonus or Penalty</h4>
+    </div>
+    <div class="bestows-btn-wrapper">
+      <button class="bestows-btn" @click="deleteDamage">Delete a Bonus or Penalty</button>
+      <button class="bestows-btn" @click="addDamage">Add a Bonus or Penalty</button>
+    </div>
+
+    <div class="head-3">
+      <h3>Additional Modifiers</h3>
     </div>
 
 
@@ -223,6 +261,19 @@ function onWheel(event, obj, key, step = 1) {
   align-items: center;
 }
 
+.head-4{
+  margin-top: 1em;
+  text-align: center;
+  border: 3px solid var(--Color4);
+  border-radius: 0.5em;
+  height: 2em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 24em;
+
+}
+
 .damage-container{
   display: flex;
   flex-direction: column;
@@ -274,11 +325,51 @@ function onWheel(event, obj, key, step = 1) {
 }
 
 #modifier-name {
-  margin-left: 8.5em;
-  width: 24.5em;
+  width: 33em;
 }
 
 #modifier-value {
-  width: 16em;
+  margin-left: 8.5em;
+  width: 7.5em;
 }
+
+.traits-container {
+  display: flex;
+  flex-direction: row;
+  margin-top: 1em;
+  gap: 1em;
+}
+
+#trait-name{
+  width: 24.5em;
+}
+
+#trait-value{
+  width: 7em;
+}
+
+.bestows-btn{
+  width: 11.5em;
+  height: 3.5em;
+  margin-top: 1em;
+  font-size: 1em;
+  font-family: inherit;
+  text-align: center;
+  border: 3px solid var(--Color3);
+  background-color: var(--Color2);
+  border-radius: 0.5em;
+}
+
+.bestows-btn:hover{
+  cursor: pointer;
+  border-color: var(--Color4);
+}
+
+.bestows-btn-wrapper{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1em;
+}
+
 </style>
